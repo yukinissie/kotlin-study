0284d560eb516a0197dbbad012f75d5e80d9fd16
yukinissie <bigtree2490@outlook.jp>
2022-02-27 22:34:44 +0900

    class test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,34 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+class Category(name: String) {
+    private var name: String
+
+    init {
+        this.name = name
+    }
+
+    fun getName(): String {
+        return this.name
+    }
+}
+
+class Todo(title: String, body: String, category: Category) {
+    private var title: String
+    private var body: String
+    private var category: Category
+
+    init {
+        this.title = title
+        this.body = body
+        this.category = category
+    }
+
+    fun printTodo() {
+        print(this.title + this.body + this.category.getName())
+    }
+}
+
+fun main() {
+    val category = Category("ブログ")
+    val todo = Todo("Kotlinの勉強", "DDDも一緒に学ぶ", category)
+    todo.printTodo()
 }
